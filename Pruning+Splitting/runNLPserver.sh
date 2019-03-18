eece3b5bc2c8cdc98a3c62ad744e12a6cb5fe9e9
Michael Roark <roarkmichael@gmail.com>
2019-03-17 23:54:02 -0700

    Fixup path to coreNLP dir

--- Pruning+Splitting/runNLPserver.sh
+++ Pruning+Splitting/runNLPserver.sh
@@ -1,3 +1,3 @@
-cd /mnt/sdb1/Pipeline/tools/stanford/stanford-corenlp-full-2016-10-31
+pushd ~/dev/stanford-corenlp-full-2018-10-05
 java -mx4g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer -port 9000 -timeout 15000
 
