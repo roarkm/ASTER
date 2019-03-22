693ed714dacdd5bb60d035a400db5017fdf1468f
Michael Roark <roarkmichael@gmail.com>
2019-03-22 15:31:52 -0700

    P&S: increase corenlp server memory limits

--- Pruning+Splitting/runNLPserver.sh
+++ Pruning+Splitting/runNLPserver.sh
@@ -1,3 +1,3 @@
 # be sure to symlink to your correct paths
 cd ../stanford-corenlp-full-2018-10-05
-java -mx4g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer -port 9000 -timeout 999999
+java -mx12g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer -port 9000 -timeout 999999
